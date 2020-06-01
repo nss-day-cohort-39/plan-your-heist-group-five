d4416f089d2e0ee5be5044a4f3e2822854ef75e2
Drew Harper <druharper@gmail.com>
2020-06-01 10:49:19 -0500

    phase 1 done

--- Program.cs
+++ Program.cs
@@ -2,12 +2,25 @@
 
 namespace plan_your_heist_group_five
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World! Drew");
-        }
+      // Print the message "Plan Your Heist!".
+      Console.WriteLine("Plan Your Heist!");
+
+      Console.WriteLine($"Enter team member's name:");
+      string TeamMemberName = Console.ReadLine();
+
+      Console.WriteLine($"Enter team member's skill level (1-10):");
+      int TeamMemberSkill = Int32.Parse((Console.ReadLine()));
+
+      Console.WriteLine($"Enter team member's courage (0.0-2.0):");
+      double TeamMemberCourage = Double.Parse((Console.ReadLine()));
+
+      Console.WriteLine($"{TeamMemberName} {TeamMemberSkill} {TeamMemberCourage}");
+
+
     }
+  }
 }
