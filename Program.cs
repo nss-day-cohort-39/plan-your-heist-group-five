cd607453d0aaa40f77738a054eb93368f38b390c
philipDmartin <philipnathan788@gmail.com>
2020-06-01 14:26:21 -0500

    added team list

--- Program.cs
+++ Program.cs
@@ -1,24 +1,64 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace plan_your_heist_group_five
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      // Print the message "Plan Your Heist!".
-      Console.WriteLine("Plan Your Heist!");
+        static void Main(string[] args)
+        {
+            List<TeamMember> Team = new List<TeamMember>();
 
-      Console.WriteLine($"Enter team member's name:");
-      string TeamMemberName = Console.ReadLine();
+            int memberCount = 0;
 
-      Console.WriteLine($"Enter team member's skill level (1-10):");
-      int TeamMemberSkill = Int32.Parse((Console.ReadLine()));
+            while (true)
+            {
+                // Print the message "Plan Your Heist!".
+                Console.WriteLine("Plan Your Heist!");
 
-      Console.WriteLine($"Enter team member's courage (0.0-2.0):");
-      double TeamMemberCourage = Double.Parse((Console.ReadLine()));
+                Console.WriteLine($"Enter team member's name:");
+                string TeamMemberName = Console.ReadLine();
 
-      Console.WriteLine($"{TeamMemberName} {TeamMemberSkill} {TeamMemberCourage}");
+                if (TeamMemberName == "")
+                {
+                    break;
+                }
+
+                Console.WriteLine($"Enter team member's skill level (1-10):");
+                int TeamMemberSkill = Int32.Parse((Console.ReadLine()));
+
+                Console.WriteLine($"Enter team member's courage (0.0-2.0):");
+                double TeamMemberCourage = Double.Parse((Console.ReadLine()));
+
+                TeamMember newMember = new TeamMember()
+                {
+                    Name = TeamMemberName,
+                    SkillLevel = TeamMemberSkill,
+                    CourageFactor = TeamMemberCourage
+                };
+
+                Team.Add(newMember);
+
+                memberCount++;
+
+                Console.WriteLine("---------------");
+
+                Console.WriteLine($"member total = {memberCount}");
+
+                Console.WriteLine("---------------");
+                Console.WriteLine($"Name: {newMember.Name}");
+
+                Console.WriteLine("---------------");
+                Console.WriteLine($"Skill Level: {newMember.SkillLevel}");
+
+                Console.WriteLine("---------------");
+                Console.WriteLine($"Courage: {newMember.CourageFactor}");
+
+                Console.WriteLine("---------------");
+
+                Console.WriteLine(Team);
+            }
+        }
     }
-  }
 }
--- TeamMember.cs
+++ TeamMember.cs
@@ -2,16 +2,10 @@ using System;
 
 namespace plan_your_heist_group_five
 {
-  public class TeamMember
-  {
-    public string Name { get; set; }
-    public double CourageFactor { get; set; }
-    public int SkillLevel { get; set; }
-    public TeamMember(string newName, double newCourage, int newSkillLevel)
+    public class TeamMember
     {
-      Name = newName;
-      CourageFactor = newCourage;
-      SkillLevel = newSkillLevel;
+        public string Name { get; set; }
+        public double CourageFactor { get; set; }
+        public int SkillLevel { get; set; }
     }
-  }
 }
